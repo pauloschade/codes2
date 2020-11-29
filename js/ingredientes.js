e0ecc885092e135378e27977302deabec65b5599
pauloschade <paulosc4@al.insper.edu.br>
2020-11-29 18:03:11 -0300

    caso de primeiros socorros

--- js/ingredientes.js
+++ js/ingredientes.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     ing = document.querySelector('.ing')
     sub =  document.querySelector('.sub-lista')
+    ing2 = document.querySelector('.ing2')
+    sub2 =  document.querySelector('.sub-lista2')
+
 
     ing.addEventListener('click', function(event) {
 
@@ -9,6 +12,12 @@ document.addEventListener('DOMContentLoaded', function() {
         ing.style.borderTop = '#686564'
         ing.style.borderBottom = '#686564'
         sub.style.display = "flex"
+    })
 
+    ing2.addEventListener('click', function(event) {
+        ing2.style.backgroundColor = '#686564'
+        ing2.style.borderTop = '#686564'
+        ing2.style.borderBottom = '#686564'
+        sub2.style.display = "flex"
     })
 })
